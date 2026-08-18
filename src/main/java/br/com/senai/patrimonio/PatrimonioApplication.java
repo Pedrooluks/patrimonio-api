e5d6e9fe48de587afe46cd0b0db2f0908b4b2d08
Pedro <pedrolucas42567@gmail.com>
2026-08-17 21:11:34 -0300

    Criado projeto

---
+++ src/main/java/br/com/senai/patrimonio/PatrimonioApplication.java
@@ -0,0 +1,37 @@
+package br.com.senai.patrimonio;
+
+import br.com.senai.patrimonio.model.Empresa;
+import br.com.senai.patrimonio.model.Endereco;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrimonioApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PatrimonioApplication.class, args);
+
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Senai LTDA");
+		System.out.println(empresa.getRazaoSocial());
+
+		Endereco endereco = new Endereco();
+		endereco.setRua("Bela vista");
+		System.out.println(endereco.getRua());
+		System.out.println(endereco.getBairro());
+
+		empresa.setEndereco(endereco);
+		System.out.println(empresa.getEndereco().getRua());
+
+		Endereco enderecoComArgumentos = new Endereco("Líbano jose gomes",
+				"489", "Perto do posto de saúde",
+				"Santa luzia","Criciúma", "SC");
+		System.out.println(enderecoComArgumentos.getBairro());
+
+
+
+
+	}
+
+}
